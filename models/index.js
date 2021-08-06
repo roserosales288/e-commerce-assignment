4e08fadc2e2b9988367a5ff37d108753729b6579
Diamond Rosales <roserosales288@gmail.com>
2021-08-06 10:40:45 -0400

    pushing homework

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:{
+    model:ProductTag,
+    unique: false
+  }
+})
+// Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(Product,{
+  through:{
+    model
+  }
+})
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
